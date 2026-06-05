68291bd63d1fc4482a1f071853e639bff8b147f6
Damandundel <damian.georgiev08@gmail.com>
2026-06-05 10:09:23 +0300

    Add constants for roles and stock transaction types
    Introduced a new namespace `InventoryManagementSystem.Data.Constants`
    and added two static classes:
    
    - `RoleConstants`: Defines the `Administrator` role constant.
    - `StockTransactionTypes`: Defines `Add` and `Remove` constants
      for stock transaction types.
    
    These changes improve code readability and maintainability by
    centralizing commonly used constants.

---
+++ InventoryManagementSystem.Data/Constants/StockTransactionTypes.cs
@@ -0,0 +1,8 @@
+namespace InventoryManagementSystem.Data.Constants
+{
+    public static class StockTransactionTypes
+    {
+        public const string Add = "Add";
+        public const string Remove = "Remove";
+    }
+}
